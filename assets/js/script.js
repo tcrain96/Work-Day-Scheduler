e30f55ef6dfd93489bd67e894c4d1bbb6f01c6e4
Tristan Crain <tcrain@metbiblechurch.local>
2021-12-10 15:36:30 -0500

    added styles to rows as well as task input and save logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,68 +1,94 @@
-//Set-up date
-$("#currentDay").text(moment().format("MMMM D, YYYY"));
+//Global Variables
+var tasks = [];
+var totalRows = 9;
 
-//Set-up Time Row
-var timeRowArray =[];
-for(var i = 0; i < 9; i++)
-{
-    //calculate the current time based off array length
-    var currentTime;
-    if((i + 9) < 12){
-        currentTime = (i + 9) + "am";
-    }
-    else if((i + 9) - 12 === 0)
-    {
-        currentTime = 12 + "pm";
+OnLoad();
+function OnLoad(){
+    createRows(totalRows);
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+    if(!tasks){
+        return;
     }
     else{
-        currentTime = (i + 9 - 12) + "pm";
+        for (var i = 0; i < totalRows; i++){
+            for(var v = 0; v < tasks.length; v++){
+                if(tasks[v].taskID === "task-"+ i){
+                    $("#task-"+i).text(tasks[v].taskText);
+                }
+            }
+        }
     }
-    
-    //create the row object
-    var timeRow = {
-        time:currentTime,
-        task:"This is a task",
-    };
-
-    //store the row object in an array
-    timeRowArray.push(timeRow);
 }
 
-for(var i = 0; i < 9; i++){
+//create the number of rows we want (in this case, 9 rows)
+function createRows(numberOfRows){
     
-    //create elements
-    var row = $("<div>").addClass("row");
-    var timeCol = $("<div>").addClass("col").append($("<p>").addClass("timeCol text-center").text(timeRowArray[i].time));
-    var taskCol = $("<div>").addClass("col-10").append($("<p>").addClass("taskCol i").text(timeRowArray[i].task));
-    var saveCol = $("<div>").addClass("col").append($("<p>").addClass("saveCol text-center").text("save"));
+    //create the rows and fill the array with each row
+    for(var i = 0; i < numberOfRows; i++)
+    {
+        var row = $("<div>").addClass("row").addClass("row-" + (i));
+        
+        var currentTime;
+        if((i + 9) < 12){
+            currentTime = (i + 9) + "am";
+        }
+        else if((i + 9) - 12 === 0){
+        currentTime = 12 + "pm";
+        }
+        else{
+        currentTime = (i + 9 - 12) + "pm";
+        }
 
-    //append elements
-    $("#task-container").append(row);
-    row.append(timeCol);
-    row.append(taskCol);
-    row.append(saveCol);
-}
+        var timeCol = $("<p>").addClass("col").addClass("timeCol").text(currentTime);
+        var taskCol = $("<p>").addClass("col-8").addClass("taskCol").attr("id","task-" + i);
+        var saveCol = $("<p>").addClass("col").addClass("saveCol").addClass("far").addClass("fa-save");
+
+        row.append(timeCol);
+        row.append(taskCol);
+        row.append(saveCol);
+        $("#task-container").append(row);
+    }
+
+};
 
 //process click within each task column
 $("#task-container").on("click",".taskCol",function(){
-    // get current text of p element
+    
     var text = $(this).text().trim();
-    
-    // replace p element with a new textarea
-    var textInput = $("<textarea>").addClass("taskCol").val(text);
-    $(this).replaceWith(textInput);
+    var id = $(this).attr("id");
+    console.log(id);
+    var inputEl = $("<input>").attr("type","text").addClass("col-8").addClass("taskCol").attr("id", id);
+    inputEl.val(text);
 
-    // auto focus new element
-    textInput.trigger("focus");
+    $(this).replaceWith(inputEl);
+
+    inputEl.trigger("focus");
 });
 
-//process click within each task column
-$("#task-container").on("blur","textarea",function(){
+$("#task-container").on("blur",".taskCol",function(){
     
-    // get current text of textarea element
-    var text = $(this).val();
+    var text = $(this).val().trim();
+    var id = $(this).attr("id");
+    var pEl = $("<p>").addClass("col-8").addClass("taskCol").attr("id",id);
+    pEl.text(text);
 
+    $(this).replaceWith(pEl);
+    var taskObj = {
+        taskID:id,
+        taskText:pEl.text()        
+    }
+    saveTaskObj(taskObj);
 });
+
+//save tasks to local storage
+function saveTaskObj(taskObj){
+    tasks.push(taskObj);
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+};
+
+//Set-up date
+$("#currentDay").text(moment().format("MMMM D, YYYY"));
+
 
 
 
