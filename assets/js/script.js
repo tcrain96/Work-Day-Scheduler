920c17d0851d5b0d8f756160d587d28bb51ad0c5
tcrain96 <tristan.crain@outlook.com>
2021-12-11 17:05:06 -0500

    added color logic to tasks as well as saving capabilities

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,12 +6,16 @@ OnLoad();
 function OnLoad(){
     createRows(totalRows);
     tasks = JSON.parse(localStorage.getItem("tasks"));
+    console.log(tasks);
     if(!tasks){
+       
         return;
     }
     else{
         for (var i = 0; i < totalRows; i++){
+            console.log("made it");
             for(var v = 0; v < tasks.length; v++){
+                
                 if(tasks[v].taskID === "task-"+ i){
                     $("#task-"+i).text(tasks[v].taskText);
                 }
@@ -39,7 +43,7 @@ function createRows(numberOfRows){
         currentTime = (i + 9 - 12) + "pm";
         }
 
-        var timeCol = $("<p>").addClass("col").addClass("timeCol").text(currentTime);
+        var timeCol = $("<p>").addClass("col").addClass("timeCol").attr("id","time-" + i).text(currentTime);
         var taskCol = $("<p>").addClass("col-8").addClass("taskCol").attr("id","task-" + i);
         var saveCol = $("<p>").addClass("col").addClass("saveCol").addClass("far").addClass("fa-save").attr("id","save-" + i);
 
@@ -51,48 +55,81 @@ function createRows(numberOfRows){
 
 };
 
+setInterval(checkTaskState(),5000);
+function checkTaskState(){
+    for(var i = 0; i < totalRows; i++){
+        if(moment($("#time-" + i).text(),"ha").isBefore())
+        {
+            $("#task-" + i).css("background-color","var(--past-color)");
+        }
+        if(moment($("#time-" + i).text(),"ha").isAfter())
+        {
+            $("#task-" + i).css("background-color","var(--present-color)");
+        }
+        if(moment($("#time-" + i).text(),"ha").isAfter(moment().add(1,"hours"))){
+            $("#task-" + i).css("background-color","var(--future-color)");
+        }
+        
+    }
+}
+
 //process click within each task column
 $("#task-container").on("click",".taskCol",function(){
     
     var text = $(this).text().trim();
     var id = $(this).attr("id");
-    console.log(id);
     var inputEl = $("<input>").attr("type","text").addClass("col-8").addClass("taskCol").attr("id", id);
     inputEl.val(text);
 
     $(this).replaceWith(inputEl);
 
     inputEl.trigger("focus");
+    checkTaskState();
 });
 
 $("#task-container").on("blur",".taskCol",function(){
     
     var text = $(this).val().trim();
     var id = $(this).attr("id");
+
     var pEl = $("<p>").addClass("col-8").addClass("taskCol").attr("id",id);
+
     pEl.text(text);
 
     $(this).replaceWith(pEl);
-    
-    var taskObj = {
-        taskID:id,
-        taskText:pEl.text()        
-    }
-    if(!tasks){
-        
-    }
-    saveTaskObj(taskObj);
+
+    checkTaskState();
 });
 
 $("#task-container").on("click",".saveCol",function(){
+    
     var saveID = $(this).attr("id").replace("save-","");
-    var taskID = $("#task-" + saveID);
-    console.log(taskID);
+    var currentTask = $("#task-" + saveID);
+
+    var taskObj = {
+        taskID:"task-" + saveID,
+        taskText:currentTask.text() 
+    }
+    
+    if(tasks.length === 0){
+        tasks.push(taskObj);
+    }
+
+    var isInList = false;
+    for(var i = 0; i < tasks.length; i++){
+        if(taskObj.taskID === tasks[i].taskID){
+            tasks[i].taskText = taskObj.taskText;
+            isInList = true;
+        }
+    }
+    if(isInList === false){
+        tasks.push(taskObj);
+    }
+    saveTaskObj();
 });
 
 //save tasks to local storage
-function saveTaskObj(taskObj){
-    tasks.push(taskObj);
+function saveTaskObj(){
     localStorage.setItem("tasks",JSON.stringify(tasks));
 };
 
